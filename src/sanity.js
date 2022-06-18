3c56da2da012620058c6a179e9e8417dd56680c3
mornir <jeromepott@hotmail.com>
2022-06-18 18:29:23 +0200

    feat: add photos

--- src/sanity.js
+++ src/sanity.js
@@ -1,10 +1,19 @@
 import PicoSanity from 'picosanity'
+import imageUrlBuilder from '@sanity/image-url'
 
-const sanity = new PicoSanity({
+const sanity = {}
+
+sanity.client = new PicoSanity({
   projectId: 'uxzcxr9o',
   dataset: 'production',
   apiVersion: '2022-06-15', // use a UTC date string
   useCdn: true,
 })
+
+const builder = imageUrlBuilder(sanity.client)
+
+sanity.urlFor = function (source) {
+  return builder.image(source)
+}
 
 export default sanity
